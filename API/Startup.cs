7820ba4ff55e0fbad2107eadcc74de645c1a3160
popmihai2707 <popmihai2707@gmail.com>
2022-10-17 17:46:09 +0300

    Added Repository Pattern

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,6 +1,6 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -15,7 +15,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
         }
